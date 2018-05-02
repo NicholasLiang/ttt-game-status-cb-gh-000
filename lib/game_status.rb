352145faaf414d7ce5c3cd9d2a93fdd7dcce1c80
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 08:14:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,8 @@ def won?(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
     
+    if position_taken?(position_1) && position_taken?(position_2) && position_taken?(position_3)
+    
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
